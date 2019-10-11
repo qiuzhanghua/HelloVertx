89e659f62b58ba19b2f3dadd08fbb4c5e2a4e98f
邱张华 <qiuzhanghua@icloud.com>
2019-10-11 11:22:15 +0800

    try another method

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,12 @@ application {
     applicationDefaultJvmArgs = listOf("-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory")
 }
 
+// not same as applicationDefaultJvmArgs, 
+//run {
+//    val systemProperties = System.getProperties()
+//    systemProperties.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory")
+//}
+
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
